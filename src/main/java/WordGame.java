4375df269cde53c6141cb0a877053f6566cd7309
Tim Herold and Tim Jung <tim+tim@>
2016-09-08 10:31:49 -0700

    create Hangman application logic

---
+++ src/main/java/WordGame.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class WordGame {
+  String mWord;
+  ArrayList<String> mGuesses;
+  String[] mWordList = {"test", "cat", "dog", "programming"};
+
+  public WordGame(String word) {
+    mWord = word;
+    mGuesses = new ArrayList<String>();
+  }
+
+  public WordGame() {
+    mGuesses = new ArrayList<String>();
+    Random myRandomGenerator = new Random();
+    Integer randomNumber = myRandomGenerator.nextInt(mWordList.length);
+    mWord = mWordList[randomNumber];
+  }
+
+  public Integer getGameOver() {
+    Integer status = 0;
+    if (getHiddenWord().indexOf("_") == -1) {
+      status = 1;
+    } else if (getWrongLetterCount() == 7) {
+      status = 2;
+    }
+    return status;
+  }
+
+  public String getWord() {
+    return mWord;
+  }
+
+  public String getHiddenWord() {
+    String output = "";
+    for (int i = 0; i < mWord.length(); i++) {
+      if (mGuesses.contains(Character.toString(mWord.charAt(i)))) {
+        output += mWord.charAt(i);
+      } else {
+        output += "_";
+      }
+    }
+    return output;
+  }
+
+  public void guessLetter(String guess) {
+    mGuesses.add(guess);
+  }
+
+  public Integer getWrongLetterCount() {
+    Integer count = 0;
+    for (String letter: mGuesses) {
+      if (mWord.indexOf(letter) == -1) {
+        count ++;
+      }
+    }
+    return count;
+  }
+}
---
+++ src/test/java/WordGameTest.java
@@ -0,0 +1,89 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class WordGameTest {
+
+  @Test
+  public void newWordGame_instanceOfWordGame() {
+    WordGame testWordGame = new WordGame("test");
+    assertEquals(true, testWordGame instanceof WordGame);
+  }
+
+  @Test
+  public void getWord_returnsOriginalWord() {
+    WordGame testWordGame = new WordGame("test");
+    String expected = "test";
+    assertEquals(expected, testWordGame.getWord());
+  }
+
+  @Test
+  public void getHiddenWord_returnsSecretWord() {
+    WordGame testWordGame = new WordGame("test");
+    String expected = "____";
+    assertEquals(expected, testWordGame.getHiddenWord());
+  }
+
+  @Test
+  public void guessLetter_guessLetterCorrectly_returnsSecretWord() {
+    WordGame testWordGame = new WordGame("test");
+    testWordGame.guessLetter("e");
+    String expected = "_e__";
+    assertEquals(expected, testWordGame.getHiddenWord());
+  }
+
+  @Test
+  public void guessLetters_guessLettersCorrectly_returnSecretWord() {
+    WordGame testWordGame = new WordGame("test");
+    testWordGame.guessLetter("e");
+    testWordGame.guessLetter("s");
+    testWordGame.guessLetter("t");
+    String expected = "test";
+    assertEquals(expected, testWordGame.getHiddenWord());
+  }
+
+  @Test
+  public void guessLetters_guessWrongLetters_returnNumberOfGuesses(){
+    WordGame testWordGame = new WordGame("test");
+    testWordGame.guessLetter("z");
+    Integer expected = 1;
+    assertEquals(expected, testWordGame.getWrongLetterCount());
+  }
+
+  @Test
+  public void getRandomWord_returnDifferentHiddenWords(){
+    WordGame testWordGame = new WordGame();
+    assertEquals(true, testWordGame.getWord() instanceof String);
+
+  }
+
+  @Test
+  public void finishGame_guessWord_getGameNotOver(){
+    WordGame testWordGame = new WordGame();
+    Integer gameNotOver = 0;
+    assertEquals(gameNotOver, testWordGame.getGameOver());
+  }
+
+  @Test
+  public void finishGame_guessWord_getGameOverPlayerWin(){
+    WordGame testWordGame = new WordGame("test");
+    Integer gameOverPlayerWin = 1;
+    testWordGame.guessLetter("e");
+    testWordGame.guessLetter("s");
+    testWordGame.guessLetter("t");
+    assertEquals(gameOverPlayerWin, testWordGame.getGameOver());
+  }
+
+  @Test
+  public void finishGame_guessWord_getGameOverPlayerLose(){
+    WordGame testWordGame = new WordGame("test");
+    Integer gameOverPlayerLost = 2;
+    testWordGame.guessLetter("z");
+    testWordGame.guessLetter("f");
+    testWordGame.guessLetter("g");
+    testWordGame.guessLetter("k");
+    testWordGame.guessLetter("l");
+    testWordGame.guessLetter("n");
+    testWordGame.guessLetter("o");    
+    assertEquals(gameOverPlayerLost, testWordGame.getGameOver());
+  }
+}
